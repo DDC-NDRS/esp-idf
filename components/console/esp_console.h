eca2d250b77bd242603435b1a442ee828212e6bc
ndrs-pst <50133174+ndrs-pst@users.noreply.github.com>
2026-04-09 17:04:35 +0700

    pst_v6.0_rebase: 2026-04-09

--- components/console/esp_console.h
+++ components/console/esp_console.h
@@ -68,7 +68,7 @@ typedef struct {
         .history_save_path = NULL,        \
         .task_stack_size = 4096,          \
         .task_priority = 2,               \
-        .task_core_id = tskNO_AFFINITY,   \
+        .task_core_id = (BaseType_t)tskNO_AFFINITY, \
         .prompt = NULL,                   \
         .max_cmdline_length = 0,          \
         .max_cmdline_args = 0,            \
@@ -127,7 +127,7 @@ typedef struct {
  *
  */
 typedef struct {
-
+    uint32_t dummy;
 } esp_console_dev_usb_serial_jtag_config_t;
 
 #define ESP_CONSOLE_DEV_USB_SERIAL_JTAG_CONFIG_DEFAULT() {}
